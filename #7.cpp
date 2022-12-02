f8e45fd14a440cc719b05b34b559523ed4c680e2
Kirikaito <116945391+Kirikaito@users.noreply.github.com>
2022-12-02 22:54:49 +0500

    Update #7.cpp

--- #7.cpp
+++ #7.cpp
@@ -4,61 +4,30 @@ using namespace std;
 
 int main()
 {
-    int m,n,k,c = 0;
-    char in,tx,ty;
+    int n, m, k;
     cin >> n >> m >> k;
-    vector<char> buff;
-    vector<vector<char>> tor1;
-    vector<vector<char>> tor2;
-    for(int i = 0;i<n;i++){
-        for(int e = 0;e<m;e++){
-            buff.push_back(in);
-        }
-        tor1.push_back(buff);
-        buff.clear();
+    vector <string> l(n), l1(n);
+    for (int z = 0; z < n; z++) { 
+        cin >> l[z];
+        l1[z] = l[z];
     }
-    for(int d = 0;d < k;d++){
-        for(int x = 0;x < m;x++){
-            for(int y = 0;y < n;y++){
-                for(int nx = x - 1;nx < x + 2;nx++){
-                    for(int ny = y - 1;ny < y + 2;ny++){
-                        if(ny < 0){
-                            ty = n - 1;
-                        }else{
-                            if(ny < n - 1 ){
-                                ty = 0;
-                            }else{
-                                ty = ny;
-                            }
-                        }
-                        if(nx < 0){
-                            tx = m - 1;
-                        }else{
-                            if(nx < m - 1 ){
-                                tx = 0;
-                            }else{
-                                tx = nx;
-                            }
-                        }
-                        if(tor1[ty][tx] == '*'){
-                            c++;
-                        }
-                    }
-                }
-                if((c == 2) or (c == 3)){
-                    tor2[y][x] = '*';
-                }else{
-                    tor2[y][x] = '.';
-                }
-                c = 0;
+    cout << endl;
+    for (int t = 0; t < k; t++) {
+        for (int z = 0; z < n; z++)
+            for (int x = 0; x < m; x++) {
+                int s = 0; 
+                s += l[(z + n - 1) % n][(x + m - 1) % m] == '*';
+                s += l[(z + n - 1) % n][x] == '*';
+                s += l[(z + n - 1) % n][(x + m + 1) % m] == '*';
+                s += l[z][(x + m - 1) % m] == '*';
+                s += l[z][(x + m + 1) % m] == '*';
+                s += l[(z + n + 1) % n][(x + m - 1) % m] == '*';
+                s += l[(z + n + 1) % n][x] == '*';
+                s += l[(z + n + 1) % n][(x + m + 1) % m] == '*';
+                if (s < 2 || s>3) l1[z][x] = '.'; 
+                if (s == 3) l1[z][x] = '*'; 
             }
-        }
-        tor1 = tor2;
+        for (int z = 0; z < n; z++) l[z] = l1[z]; 
     }
-    for(auto i : tor1){
-        for(auto e : i){
-            cout << e;
-        }
-        cout<<endl;
-    }
+    for (int z = 0; z < n; z++) cout << l[z] << endl; 
 }
