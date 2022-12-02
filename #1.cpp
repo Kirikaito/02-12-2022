9ffa7b5565b81460863909e59a3affcbe8f3c344
Kirikaito <116945391+Kirikaito@users.noreply.github.com>
2022-12-02 17:37:21 +0500

    Update #1.cpp

--- #1.cpp
+++ #1.cpp
@@ -12,29 +12,22 @@ int main()
     
     for(int i = 0; i<n; i++){
         cin >> lis[i];
+        if(lis[i] == 10){
+            r++;
+        }else{
+            r += 0.5;
+        }
     }
     
     
     sum += lis[0] + lis[1];
     if(lis[0] == 10){
         sum += lis[1];
-        r++;
-    }else{
-        r += 0.5;
-    }
-    if(lis[1] == 10){
-        r++;
-    }else{
-        r += 0.5;
     }
     
     for(int i = 2; i<n; i++){
         sum += lis[i];
-        if(lis[i] == 10){
-            r++;
-        }else{
-            r += 0.5;
-        }
+        
         if(lis[i - 2] == 10){
             sum +=lis[i];
         }else{
@@ -44,20 +37,16 @@ int main()
         }
         if(lis[i - 1] == 10){
             sum +=lis[i];
-        }else{
-            if(lis[i - 1] + lis[i - 2] == 10){
-            sum +=lis[i];
-            }
         }
     }
     if(r > 10){
         if(lis[n - 3] == 10){
-            sum -= lis[n - 2] + lis[n - 1];
+            sum = sum - lis[n - 2] - lis[n - 1];
             if(lis[n - 4] == 10){
-                sum -= lis[n - 2];
+                sum = sum - lis[n - 2];
             }
         }else{
-            sum -= lis[n - 1];
+            sum = sum - lis[n - 1];
         }
     }
     cout << sum;
